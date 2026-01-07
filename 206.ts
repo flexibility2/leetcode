17517d5dcecc0c31817fef4f0da72c439555f3f1
flexibility2 <729406307@qq.com>
2026-01-07 21:53:31 +0800

    206 Reverse List

---
+++ 206.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let pre = null;
+  while (head) {
+    let next = head.next;
+    head.next = pre;
+    pre = head;
+    head = next;
+  }
+  return pre;
+}
