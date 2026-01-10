ade21dcc9e291ff8bec25dd38b36d738fff5bae4
flexibility2 <729406307@qq.com>
2026-01-10 20:43:35 +0800

    Implement solution for the House Robber problem

---
+++ 198.ts
@@ -0,0 +1,10 @@
+function rob(nums: number[]): number {
+  let n = nums.length;
+  let dp: number[] = new Array(n).fill(0);
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < n; i++) {
+    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+  }
+  return dp[n - 1];
+}
